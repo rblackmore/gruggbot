31dff37011829def1c144a790fc87e381c26d075
Ryan Blackmore <ryan@ryanblackmore.com>
2018-07-10 09:52:18 +1000

    Basic Commands Added

--- Core/Gruggbot.Core/BotApp.cs
+++ Core/Gruggbot.Core/BotApp.cs
@@ -1,5 +1,7 @@
-﻿using Discord;
+﻿ using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Gruggbot.Core.CommandModules;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,33 +15,63 @@ namespace Gruggbot.Core
     {
         private IConfigurationRoot _configuration;
         private ILogger<BotApp> _logger;
+        private IServiceProvider _serviceProvider;
         private DiscordSocketClient _discordClient;
+        private CommandService _commands;
 
-        public BotApp(ILogger<BotApp> logger, IConfigurationRoot configuration, DiscordSocketClient discordClient)
+        public BotApp(ILogger<BotApp> logger, IConfigurationRoot configuration, DiscordSocketClient discordClient, IServiceProvider serviceProvider)
         {
             _configuration = configuration;
             _logger = logger;
+            _serviceProvider = serviceProvider;
             _discordClient = discordClient;
+            _commands = new CommandService();
         }
 
         public async Task Run()
         {
             _discordClient.Log += Log;
-            _discordClient.MessageReceived += MessageRecieved;
+
+            await InstallCommands();
 
             string token = _configuration.GetSection("Token").Value;
             await _discordClient.LoginAsync(TokenType.Bot, token);
             await _discordClient.StartAsync();
+
+            await Task.Delay(-1);
         }
 
-        private async Task MessageRecieved(SocketMessage message)
+        private async Task InstallCommands()
         {
-            if (!message.Author.IsBot)
-                _logger.LogInformation("Message Received from {@Username} in {@Channel}: {@Content}", message.Author.Username, message.Channel.Name, message.Content);
+            _discordClient.MessageReceived += HandleCommand;
 
+            //Add Modules
+            await _commands.AddModuleAsync<InfoCommandModule>();
+        }
 
-            if (message.Content == "!ping")
-                await message.Channel.SendMessageAsync("Pong!!!");
+        private async Task HandleCommand(SocketMessage msg)
+        {
+            if (!(msg is SocketUserMessage message))
+                return;
+
+            if (message.Author.IsBot)
+                return;
+
+            //First character of command after prefix
+            int argPos = 0;
+
+            if (!(message.HasCharPrefix('!', ref argPos) || !message.HasMentionPrefix(_discordClient.CurrentUser, ref argPos)))
+                return;
+
+            //Create Command Context
+            var context = new CommandContext(_discordClient, message);
+
+            //Execute the command. (result does not indicate a return value,
+            //rather an object stating if the command executed successfully
+            var result = await _commands.ExecuteAsync(context, argPos, _serviceProvider);
+
+            if (!result.IsSuccess)
+                _logger.LogError("{@error} {@message}", result.ErrorReason, message.Content);
         }
 
         private Task Log(LogMessage msg)
---
+++ Core/Gruggbot.Core/CommandModules/InfoCommandModule.cs
@@ -0,0 +1,25 @@
+﻿using Discord;using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruggbot.Core.CommandModules
+{
+    public class InfoCommandModule : ModuleBase
+    {
+        [Command("say"), Summary("Echos a message.")]
+        public async Task Say([Remainder, Summary("The text to echo")] string echo)
+        {
+            await ReplyAsync(echo);
+        }
+
+        [Command("userinfo"), Summary("Returns info about the current user, or the user parameter, if one is parsed")]
+        [Alias("user", "whois")]
+        public async Task UserInfo([Summary("The (optional) user to get info for")] IUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+    }
+}
