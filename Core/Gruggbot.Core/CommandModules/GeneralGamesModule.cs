f7e11f07d52c5172028dca25d998f279bc5a314e
Ryan Blackmore <rblackmore.ryan@gmail.com>
2020-03-25 13:01:54 +1100

    General Games Module

---
+++ Core/Gruggbot.Core/CommandModules/GeneralGamesModule.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruggbot.Core.CommandModules
+{
+    [Summary("Games")]
+    public class GeneralGamesModule : ModuleBase
+    {
+        public async Task Roll(int max = 6, int min = 1)
+        {
+            Random rando = new Random();
+
+            await Context.Channel.SendMessageAsync(rando.Next(min, max).ToString());
+        }
+    }
+}
