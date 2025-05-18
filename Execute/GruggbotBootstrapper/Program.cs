5195a735e326df5b9a559b50e394812228e33775
Ryan Blackmore <3052414+rblackmore@users.noreply.github.com>
2025-05-18 16:48:36 +1000

    App Configuration Adjusted.
    App Configuration is now done for early logging, and then again in teh same way for the Host, this way, I can put appsettings.json files in /config within the app working directly, and it can be mapped to a docker container, instead of using appsettings.json from the root working directory.

--- Execute/GruggbotBootstrapper/Program.cs
+++ Execute/GruggbotBootstrapper/Program.cs
@@ -2,67 +2,73 @@
 // Copyright © 2020 Ryan Blackmore. All rights Reserved.
 // </copyright>
 
-namespace GruggbotBootstrapper
+namespace GruggbotBootstrapper;
+
+using System;
+using System.Threading.Tasks;
+
+using Gruggbot.DependencyInjection;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+
+public static class Program
 {
-    using System;
-    using System.IO;
-    using System.Threading.Tasks;
-    using Gruggbot.DependencyInjection;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Hosting;
-    using Serilog;
-
-    public static class Program
+  public static async Task Main(string[] args)
+  {
+    try
     {
-        public static async Task Main(string[] args)
-        {
-            try
-            {
-                ConfigureLogging();
-                
-                await CreateHostBuilder(args).Build().RunAsync().ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                Log.Logger.Fatal(ex.Message);
-            }
-        }
+      ConfigureLogging();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((context, services) =>
-                {
-                    // services.AddHostedService<App>();
-                    services.AddBot(context.Configuration);
-                })
-                .UseSerilog((context, loggerConfiguration) =>
-                {
-                    loggerConfiguration.ReadFrom.Configuration(context.Configuration);
-                });
-
-        public static IConfigurationBuilder CreateConfigurationBuilder()
-        {
-            var builder = new ConfigurationBuilder();
-
-            var env_settings =
-                $"appsettings.{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? Environments.Production}.json";
-
-            builder.SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile(env_settings, optional: true)
-                .AddEnvironmentVariables();
-
-            return builder;
-        }
-
-        public static void ConfigureLogging()
-        {
-            var builder = CreateConfigurationBuilder();
-            var config = builder.Build();
-
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(config)
-                .CreateLogger();
-        }
+      await CreateHostBuilder(args).Build().RunAsync().ConfigureAwait(false);
     }
+    catch (Exception ex)
+    {
+      Log.Logger.Fatal(ex.Message);
+    }
+  }
+
+  public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+      .ConfigureHostConfiguration(ConfigureConfigurationBuilder)
+      .ConfigureServices((context, services) =>
+      {
+        // services.AddHostedService<App>();
+        services.AddBot(context.Configuration);
+      })
+      .UseSerilog((context, loggerConfiguration) =>
+      {
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+      });
+
+  /// <summary>
+  /// Configures the Configuration Builder.
+  /// </summary>
+  /// <returns>Configuration Builder.</returns>
+  public static void ConfigureConfigurationBuilder(IConfigurationBuilder builder)
+  {
+    var envSettings =
+      $"appsettings.{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? Environments.Production}.json";
+
+    builder.SetBasePath($"{AppDomain.CurrentDomain.BaseDirectory}/config")
+      .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+      .AddJsonFile(envSettings, optional: true)
+      .AddEnvironmentVariables();
+  }
+
+  /// <summary>
+  /// Configures Serilog for use before the Host is built.
+  /// </summary>
+  public static void ConfigureLogging()
+  {
+    var builder = new ConfigurationBuilder();
+    ConfigureConfigurationBuilder(builder);
+    var config = builder.Build();
+
+    Log.Logger = new LoggerConfiguration()
+      .ReadFrom.Configuration(config)
+      .CreateLogger();
+  }
 }
